6fe9b3e4d43d6cb93f46655151051caba4b73eca
Douglas Dachicourt <8815@holbertonstudents.com>
2024-04-11 11:32:21 +0000

    file 0 added

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * bubble_sort - fct that swap integers according final ascending orders
+ * @array: array of integers to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		print_array(array, size);
+
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+
+				array[j + 1] = temp;
+			}
+		}
+	}
+}
